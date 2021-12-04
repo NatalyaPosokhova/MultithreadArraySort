3532aed1905effb6ed0c85ae33265463d3c607d5
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-12-04 15:04:00 +0500

    Start work with second version.

---
+++ MultithreadArraySort/MultithreadMergeSort2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadArraySort
+{
+    public static class MultithreadMergeSort2
+    {
+        public static Task<int[]> SortAsync(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task<int[]> SortImplAsync(int[] array, Part part)
+        {
+            throw new NotImplementedException();
+        }
+
+        private enum Part
+        {
+            Left,
+            Right
+        }
+
+        public static int[] Merge(int[] leftArray, int[] rightArray)
+        {
+            int[] array = new int[leftArray.Length + rightArray.Length];
+
+            int i = 0;
+            int j = 0;
+            for (int k = 0; k < array.Length; k++)
+            {
+                if (j >= rightArray.Length)
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                    continue;
+                }
+
+                if (i >= leftArray.Length)
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                    continue;
+                }
+
+                if (leftArray[i] < rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+            }
+
+            return array;
+        }
+    }    
+}
